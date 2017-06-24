daf6aae185178e261009ae0ec3fc830ac12d9115
andrew <hunkywei@gmail.com>
2017-06-24 10:40:49 -0500

    first attempt

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -16,27 +16,49 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
                             const unsigned long _shared_size)
 {
-   assert(false);
+    PageTable::kernel_mem_pool = _kernel_mem_pool; //pointers
+    PageTable::process_mem_pool = _process_mem_pool;
+    PageTable::shared_size = _shared_size;//size of shared kernel memory
+
    Console::puts("Initialized Paging System\n");
 }
 
 PageTable::PageTable()
 {
-   assert(false);
+   page_directory = ( unsigned long*)(kernel_mem_pool->get_frame(1)*FRAME_SIZE);
+   unsigned long* page_table= (unsigned long*)(kernel_mem_pool->get_frame(1)*FRAME_SIZE);
+   unsigned long address=0; // holds the physical address of where a page is
+
+   // map the first 4MB of memory for page table
+    for(unsigned int i=0; i<1024; i++){
+      page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
+      address = address + FRAME_SIZE ; // 4096 = 4kb
+    }
+
+    page_directory[0] = (unsigned long) page_table;
+    page_directory[0] |= 3;
+
+    for(unsigned int i=1; i<1024; i++){
+      page_directory[i] = 0 | 2; // attribute set to: supervisor level, read/write, not present(010 in binary)
+    };
+
    Console::puts("Constructed Page Table object\n");
 }
 
 
 void PageTable::load()
 {
-   assert(false);
+   current_page_table  = this 
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-   assert(false);
-   Console::puts("Enabled paging\n");
+    write_cr3((unsigned long)page_directory);
+    paging_enabled = 1;
+    write_cr0(read_cr0() | 0x80000000);
+
+       Console::puts("Enabled paging\n");
 }
 
 void PageTable::handle_fault(REGS * _r)
