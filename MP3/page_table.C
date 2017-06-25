aab419d05ac638da7597222d865744f7086bf1b7
410 <guest@TA-virtualbox.(none)>
2017-06-25 12:42:00 -0500

    handle page_fault

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -3,14 +3,11 @@
 #include "console.H"
 #include "paging_low.H"
 #include "page_table.H"
-
 PageTable * PageTable::current_page_table = NULL;
 unsigned int PageTable::paging_enabled = 0;
 ContFramePool * PageTable::kernel_mem_pool = NULL;
 ContFramePool * PageTable::process_mem_pool = NULL;
 unsigned long PageTable::shared_size = 0;
-
-
 
 void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
                             ContFramePool * _process_mem_pool,
@@ -22,17 +19,20 @@ void PageTable::init_paging(ContFramePool * _kernel_mem_pool,
 
    Console::puts("Initialized Paging System\n");
 }
+unsigned long PageTable::get_page_directory(){
+	return (unsigned long) page_directory;
+}
 
 PageTable::PageTable()
 {
-   page_directory = ( unsigned long*)(kernel_mem_pool->get_frame(1)*FRAME_SIZE);
-   unsigned long* page_table= (unsigned long*)(kernel_mem_pool->get_frame(1)*FRAME_SIZE);
+   page_directory = ( unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
+   unsigned long* page_table= (unsigned long*)(kernel_mem_pool->get_frames(1)*PAGE_SIZE);
    unsigned long address=0; // holds the physical address of where a page is
 
    // map the first 4MB of memory for page table
     for(unsigned int i=0; i<1024; i++){
       page_table[i] = address | 3; // attribute set to: supervisor level, read/write, present(011 in binary)
-      address = address + FRAME_SIZE ; // 4096 = 4kb
+      address = address + PAGE_SIZE ; // 4096 = 4kb
     }
 
     page_directory[0] = (unsigned long) page_table;
@@ -48,13 +48,13 @@ PageTable::PageTable()
 
 void PageTable::load()
 {
-   current_page_table  = this 
+   current_page_table  = this;
    Console::puts("Loaded page table\n");
 }
 
 void PageTable::enable_paging()
 {
-    write_cr3((unsigned long)page_directory);
+    write_cr3(current_page_table->get_page_directory());
     paging_enabled = 1;
     write_cr0(read_cr0() | 0x80000000);
 
@@ -63,7 +63,6 @@ void PageTable::enable_paging()
 
 void PageTable::handle_fault(REGS * _r)
 {
-  assert(false);
   Console::puts("handled page fault\n");
 }
 
