4207c9cbc9e02b126d1f728f53504aefb425c3f3
andrew <hunkywei@gmail.com>
2017-06-25 19:41:51 -0500

    page_fault done

--- MP3/page_table.C
+++ MP3/page_table.C
@@ -63,6 +63,51 @@ void PageTable::enable_paging()
 
 void PageTable::handle_fault(REGS * _r)
 {
+
+    unsigned long address = read_cr2();
+    unsigned long* page_dir = current_page_table->page_directory;
+    unsigned int err_code = _r->err_code;
+    unsigned long *new_page_table;
+
+
+ /*  KERNEL_READ_PAGE_NOT_PRESENT 0
+   KERNEL_WRITE_PAGE_NOT_PRESENT 2
+   USER_READ_PAGE_NOT_PRESENT 4
+   USER_WRITE_PAGE_NOT_PRESENT 6
+
+   KERNEL_READ_PROTECTION_FAULT 1
+   KERNEL_WRITE_PROTECTION_FAULT 3
+   USER_READ_PROTECTION_FAULT 5
+   USER_WRITE_PROTECTION_FAULT 7 */
+
+
+    if(err_code & 1 == 0){ //not present fault 
+         
+         if(page_dir[address>>22] & 1 == 1){  //directory has that page table, fault happend in table
+            unsigned long page_dir_address = address>>22;
+            unsigned long page_tab_address = address>>12;
+
+            new_page_table = (unsigned long *)((page_dir[page_dir_address]>>12)<<12);
+            new_page_table[page_tab_address & 0x03FF] =  PageTable::process_mem_pool->get_frame(1)*PAGE_SIZE | 3;
+            //setting 011 is kernel r/w and present
+            //&0x03ff to get 10 bit
+          }
+
+         else{//need new table in this directory
+            unsigned long page_dir_address = address>>22;
+            unsigned long page_tab_address = address>>12;
+
+            page_dir[page_dir_address] = (unsigned long)(kernel_mem_pool->get_frame(1)*PAGE_SIZE | 3);
+            new_page_table = (unsigned long *)((page_dir[page_dir_address]>>12)<<12);
+
+            for(int i=0; i<1024; i++){
+               new_page_table[i] = 4; //user level bit on init
+            }
+
+             new_page_table[page_tab_address & 0x03FF] =  PageTable::process_mem_pool->get_frame(1)*PAGE_SIZE | 3;
+          }
+      }
+    
   Console::puts("handled page fault\n");
 }
 
