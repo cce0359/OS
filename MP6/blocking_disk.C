5a1e6a6032bea8454b427b4a78f1ea61572ae27e
andrew <hunkywei@gmail.com>
2017-07-25 19:41:42 -0500

    thread-safe disk

--- MP6/blocking_disk.C
+++ MP6/blocking_disk.C
@@ -32,16 +32,38 @@ extern Scheduler* SYSTEM_SCHEDULER;
 /* CONSTRUCTOR */
 /*--------------------------------------------------------------------------*/
 
-BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size) 
+BlockingDisk::BlockingDisk(DISK_ID _disk_id, unsigned int _size, Scheduler *_scheduler) 
   : SimpleDisk(_disk_id, _size) {
+    this->scheduler = _scheduler;
+    size = 0;
 }
 
 void BlockingDisk::wait_until_ready(){
-        while(!is_ready()){
-        	SYSTEM_SCHEDULER->add(Thread::CurrentThread());
-          	SYSTEM_SCHEDULER->yield();
-        }
+       if(!is_ready()) {
+        Thread *th = Thread::CurrentThread();
+        this->add_to_queue(th);
+        this->scheduler->yield();
+      }
+  }
+
+  void BlockingDisk::add_to_queue(Thread *_thread) {
+
+    ready_queue.enqueue(_thread);//add thread to queue
+    ++size;
 }
+void BlockingDisk::resume_from_queue() {
+  if(size == 0) {
+    Console::puts("Error: Blocked queue is NULL.\n");
+  } 
+  else {
+    Thread *th = this->blocked_thread_queue_head.dequeue();
+    this->scheduler->resume(th); 
+  }
+}
+BOOLEAN BlockingDisk::is_ready() {
+  return SimpleDisk::is_ready();
+}
+
 /*--------------------------------------------------------------------------*/
 /* SIMPLE_DISK FUNCTIONS */
 /*--------------------------------------------------------------------------*/
