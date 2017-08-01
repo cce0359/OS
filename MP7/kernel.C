ed4da56b91e8e4a56fa2e6b34ad2e2373099e2c8
andrew <hunkywei@gmail.com>
2017-08-01 11:39:44 -0500

    change

--- MP7/kernel.C
+++ MP7/kernel.C
@@ -107,7 +107,7 @@ Scheduler * SYSTEM_SCHEDULER = new Scheduler();
 /*--------------------------------------------------------------------------*/
 
 /* -- A POINTER TO THE SYSTEM DISK */
-SimpleDisk * SYSTEM_DISK;
+BlockingDisk * SYSTEM_DISK;
 
 #define SYSTEM_DISK_SIZE (10 MB)
 
@@ -349,7 +349,7 @@ int main() {
 
     /* -- DISK DEVICE -- */
 
-    SYSTEM_DISK = new SimpleDisk(MASTER, SYSTEM_DISK_SIZE);
+    SYSTEM_DISK = new BlockingDisk(MASTER, SYSTEM_DISK_SIZE);
     
     /* NOTE: The timer chip starts periodically firing as 
              soon as we enable interrupts.
